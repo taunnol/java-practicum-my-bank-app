25ef9dee11274a4b88e4e861b36a835a086b4184
taunnol <rooimeecarp@gmail.com>
2026-03-02 03:57:06 +0300

    add services draft

---
+++ cash/src/test/java/ru/yandex/practicum/bank/cash/CashApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.bank.cash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ notifications/src/test/java/ru/yandex/practicum/bank/notifications/NotificationsApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.bank.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
