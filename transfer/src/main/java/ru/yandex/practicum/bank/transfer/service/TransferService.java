bbfa408b474111dc6c4b215a879c67cafc71c182
taunnol <rooimeecarp@gmail.com>
2026-03-03 07:04:05 +0300

    add circuit breaker for transfer service

--- transfer/src/main/java/ru/yandex/practicum/bank/transfer/service/TransferService.java
+++ transfer/src/main/java/ru/yandex/practicum/bank/transfer/service/TransferService.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.bank.transfer.service;
 
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.bank.transfer.client.AccountsClient;
 import ru.yandex.practicum.bank.transfer.client.NotificationsClient;
@@ -11,10 +12,14 @@ public class TransferService {
 
     private final AccountsClient accountsClient;
     private final NotificationsClient notificationsClient;
+    private final CircuitBreakerFactory<?, ?> circuitBreakerFactory;
 
-    public TransferService(AccountsClient accountsClient, NotificationsClient notificationsClient) {
+    public TransferService(AccountsClient accountsClient,
+                           NotificationsClient notificationsClient,
+                           CircuitBreakerFactory<?, ?> circuitBreakerFactory) {
         this.accountsClient = accountsClient;
         this.notificationsClient = notificationsClient;
+        this.circuitBreakerFactory = circuitBreakerFactory;
     }
 
     public void transfer(String fromLogin, String toLogin, long amount) {
@@ -28,10 +33,12 @@ public class TransferService {
             throw new IllegalArgumentException("Нельзя переводить самому себе");
         }
 
-        accountsClient.withdraw(fromLogin, amount);
+        circuitBreakerFactory.create("accounts-withdraw").run(
+                () -> { accountsClient.withdraw(fromLogin, amount); return null; });
 
         try {
-            accountsClient.deposit(toLogin, amount);
+            circuitBreakerFactory.create("accounts-deposit").run(
+                    () -> { accountsClient.deposit(toLogin, amount); return null; });
         } catch (RuntimeException e) {
             try {
                 accountsClient.deposit(fromLogin, amount);
@@ -40,12 +47,12 @@ public class TransferService {
             throw e;
         }
 
-        notificationsClient.send(new NotificationEvent(
-                "TRANSFER",
-                amount,
-                fromLogin,
-                toLogin,
-                OffsetDateTime.now()
-        ));
+        circuitBreakerFactory.create("notifications").run(
+                () -> {
+                    notificationsClient.send(new NotificationEvent(
+                            "TRANSFER", amount, fromLogin, toLogin, OffsetDateTime.now()));
+                    return null;
+                },
+                throwable -> null);
     }
 }
