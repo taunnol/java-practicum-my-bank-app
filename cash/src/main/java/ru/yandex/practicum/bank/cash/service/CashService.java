ac7533101f1c017b2eb02310a138c804776e26f8
taunnol <rooimeecarp@gmail.com>
2026-03-03 07:03:18 +0300

    add circuit breaker for cash service

--- cash/src/main/java/ru/yandex/practicum/bank/cash/service/CashService.java
+++ cash/src/main/java/ru/yandex/practicum/bank/cash/service/CashService.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.bank.cash.service;
 
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.bank.cash.api.dto.CashAction;
 import ru.yandex.practicum.bank.cash.client.AccountsClient;
@@ -12,10 +13,14 @@ public class CashService {
 
     private final AccountsClient accountsClient;
     private final NotificationsClient notificationsClient;
+    private final CircuitBreakerFactory<?, ?> circuitBreakerFactory;
 
-    public CashService(AccountsClient accountsClient, NotificationsClient notificationsClient) {
+    public CashService(AccountsClient accountsClient,
+                       NotificationsClient notificationsClient,
+                       CircuitBreakerFactory<?, ?> circuitBreakerFactory) {
         this.accountsClient = accountsClient;
         this.notificationsClient = notificationsClient;
+        this.circuitBreakerFactory = circuitBreakerFactory;
     }
 
     public void apply(String login, long value, CashAction action) {
@@ -24,11 +29,19 @@ public class CashService {
         }
 
         if (action == CashAction.PUT) {
-            accountsClient.deposit(login, value);
-            notificationsClient.send(new NotificationEvent("CASH_IN", value, login, null, OffsetDateTime.now()));
+            circuitBreakerFactory.create("accounts-deposit").run(
+                    () -> { accountsClient.deposit(login, value); return null; });
+            sendNotification(new NotificationEvent("CASH_IN", value, login, null, OffsetDateTime.now()));
         } else {
-            accountsClient.withdraw(login, value);
-            notificationsClient.send(new NotificationEvent("CASH_OUT", value, login, null, OffsetDateTime.now()));
+            circuitBreakerFactory.create("accounts-withdraw").run(
+                    () -> { accountsClient.withdraw(login, value); return null; });
+            sendNotification(new NotificationEvent("CASH_OUT", value, login, null, OffsetDateTime.now()));
         }
+    }
+
+    private void sendNotification(NotificationEvent event) {
+        circuitBreakerFactory.create("notifications").run(
+                () -> { notificationsClient.send(event); return null; },
+                throwable -> null);
     }
 }
